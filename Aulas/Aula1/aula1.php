c540b46221e3dc24759ff3e237e6805b2cde42b5
JoseC14 <87765070+JoseC14@users.noreply.github.com>
2021-08-15 09:57:22 -0300

    Removido Aulas/aula1.html e adicionado Aulas/aula1.php

---
+++ Aulas/Aula1/aula1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php
+         $escola="Amélia Figueiredo de Lavor";
+         $quantAlun=688;
+         $quantProf=13;
+         
+         echo "escola: ".$escola."<br>";
+         echo "quantidade de alunos :".$quantAlun."<br>";
+         echo "quantidade de professores: ".$quantProf."<br>";
+         
+        
+        ?>
+    </body>
+</html>
+
+
