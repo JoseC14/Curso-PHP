fc35cd9475d00ff41e75f452a31638d8e513c4db
JoseC14 <87765070+JoseC14@users.noreply.github.com>
2021-08-31 20:39:04 -0300

    Adicionado aula4.php

--- Aulas/aula3.php
+++ Aulas/aula3.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Operações com Varíaveis</title>
-</head>
-<body>
     <?php
     
       $num1=10;
@@ -42,7 +33,7 @@
 
       echo $num1."<br>";//Resultado:1
 
-      
+
 
       
       
@@ -55,11 +46,4 @@
 
       echo $nome1."<br>";//Resultado:José Carlos Morais da Silva
 
-      
-      
-       
-    
-
-      ?>
-</body>
-</html>
+?>
---
+++ Aulas/aula4.php
@@ -0,0 +1,36 @@
+<?php
+
+$transportes=array(5);
+
+$dados=array(
+    
+    "nome"=>"José",
+    "cidade"=>"Ceará",
+    "curso" =>"PHP"
+
+);
+
+$carros=array(
+    
+    array("Sedan",65000),
+    array("Palio",35000),
+    array("Nissan",50000)
+);
+
+$i=6;
+
+$transportes[0]="Carro";
+$transportes[1]="Avião";
+$transportes[2]="Navio";
+$transportes[3]="Moto";
+$transportes[4]="Onibus";
+$transportes[5]=123456;
+$transportes[6]=$dados["nome"];
+
+echo "Elemento de Indíce $i = ".$transportes[$i]."<br>";
+
+echo "Nome: ".$dados["nome"]." - Cidade: ".$dados["cidade"]." - Curso: ".$dados["curso"]."<hr>";
+
+echo "Carro A ".$carros[0][0]." - Valor: ".$carros[0][1]."<br>";
+echo "Carro B ".$carros[1][0]." - Valor: ".$carros[1][1]."<br>";
+echo "Carro C ".$carros[2][0]." - Valor: ".$carros[2][1]."<br>";
