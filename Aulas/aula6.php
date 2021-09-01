7eaa82e6d68b5b0e39d7420629ac4d9db6bc8996
JoseC14 <87765070+JoseC14@users.noreply.github.com>
2021-09-01 08:46:42 -0300

    Adicionado aula7.php

---
+++ Aulas/aula6.php
@@ -0,0 +1,19 @@
+<?php
+
+$pos=1;
+
+switch($pos){
+
+    case 1:
+        echo "Primeiro ";
+        break;
+    case 2:
+        echo "Segundo";
+        break;
+    case 3:
+        echo "Terceiro";
+        break;
+    default:
+        echo "Não foi ao pódio";
+        break;
+};
