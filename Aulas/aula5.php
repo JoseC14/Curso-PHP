275e56f29b0587c2ac6bf1ab9f79926f3a819ddb
JoseC14 <87765070+JoseC14@users.noreply.github.com>
2021-09-01 08:15:44 -0300

    Adicionado aula5.php

---
+++ Aulas/aula5.php
@@ -0,0 +1,23 @@
+<?php
+/*
+$nota=8;
+
+if($nota>6&&$nota<10){
+    echo "Aprovado";
+}
+else if($nota==10){
+    echo "Aprovado com nota perfeita";
+}
+else if($nota>0&&$nota<6){
+    echo"Reprovado";
+}
+else{
+    echo "Nota inválida";
+}Resultado:Aprovado*/
+
+$var1=true;
+$var2=false;
+
+if($var1||$var2=false){
+    echo "Uma das varíaveis é falsa";
+}
